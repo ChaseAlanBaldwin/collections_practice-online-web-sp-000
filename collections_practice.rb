dda497160a1e5b806b13ed927244c5d8f8317f0c
Chase-Alan Baldwin <alan.chasebaldwin@gmail.com>
2020-11-12 21:13:25 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -48,7 +48,7 @@ def find_a(array)
   array.each do |x|
     string = x.split(//)
     if string[0] = 'A' || 'a'
-      wordsWithA << string
+      wordsWithA << string.join()
     end
   end
   puts wordsWithA
